a4f540e947dd87e1d33e38ee9de13aada89cea65
Alf Watt <alf@istumbler.net>
2014-09-22 12:16:23 -0700

    Initial Commit

---
+++ QRKit/QRKit.h
@@ -0,0 +1,19 @@
+//
+//  QRKit.h
+//  QRKit
+//
+//  Created by alf on 9/22/14.
+//  Copyright (c) 2014 iStumbler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRKit.
+FOUNDATION_EXPORT double QRKitVersionNumber;
+
+//! Project version string for QRKit.
+FOUNDATION_EXPORT const unsigned char QRKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRKit/PublicHeader.h>
+
+
---
+++ QRKitTests/QRKitTests.m
@@ -0,0 +1,40 @@
+//
+//  QRKitTests.m
+//  QRKitTests
+//
+//  Created by alf on 9/22/14.
+//  Copyright (c) 2014 iStumbler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface QRKitTests : XCTestCase
+
+@end
+
+@implementation QRKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
